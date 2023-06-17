378fc7af92b41c5ce60ef27242e35fa8f8d37f72
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-06-17 10:42:23 +0100

    Initial commit

---
+++ Magic x 40k/Magic x 40k/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Magic x 40k
+//
+//  Created by Jonathan Copeland on 17/06/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Magic x 40k/Magic x 40k/Magic_x_40kApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_x_40kApp.swift
+//  Magic x 40k
+//
+//  Created by Jonathan Copeland on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Magic_x_40kApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
