2f080a037501aa489778fbccaf99d746e154d927
Jonathan Copeland <10074185+JonathanCopeland@users.noreply.github.com>
2023-09-09 22:42:15 +0100

    Updated views

---
+++ Magic x 40k/Magic x 40k/Card/RarityView.swift
@@ -0,0 +1,18 @@
+//
+//  RarityView.swift
+//  Magic x 40k
+//
+//  Created by Jonathan Copeland on 09/09/2023.
+//
+
+import SwiftUI
+
+struct RarityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RarityView()
+}
---
+++ Magic x 40k/Magic x 40k/CardData.swift
@@ -0,0 +1,8 @@
+//
+//  CardData.swift
+//  Magic x 40k
+//
+//  Created by Jonathan Copeland on 09/09/2023.
+//
+
+import Foundation
